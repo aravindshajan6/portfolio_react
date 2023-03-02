14901a5d1f8c7821d27e1eca25dfdcfe6327edd5
aravindshajan6 <117029956+aravindshajan6@users.noreply.github.com>
2023-03-02 19:46:31 +0530

    Updates

---
+++ precache-manifest.b60f24a46a28b59e71ce8ef0fd8be995.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9730c2d5870bca43192f",
+    "url": "/portfolio_react/static/css/main.18858895.chunk.css"
+  },
+  {
+    "revision": "9730c2d5870bca43192f",
+    "url": "/portfolio_react/static/js/main.9730c2d5.chunk.js"
+  },
+  {
+    "revision": "9e0b21fe164afb870c62",
+    "url": "/portfolio_react/static/js/1.9e0b21fe.chunk.js"
+  },
+  {
+    "revision": "c1aa54b09c4ebbf94421",
+    "url": "/portfolio_react/static/css/2.93c552f0.chunk.css"
+  },
+  {
+    "revision": "c1aa54b09c4ebbf94421",
+    "url": "/portfolio_react/static/js/2.c1aa54b0.chunk.js"
+  },
+  {
+    "revision": "b02ea540fdea2025c15a",
+    "url": "/portfolio_react/static/js/runtime~main.b02ea540.js"
+  },
+  {
+    "revision": "da07a3d302c56ef4e0c8b06c88b820e2",
+    "url": "/portfolio_react/static/media/portfolio_img.da07a3d3.jpg"
+  },
+  {
+    "revision": "acc537bf868ea4301089ad9eb6b1dc2e",
+    "url": "/portfolio_react/index.html"
+  }
+];
